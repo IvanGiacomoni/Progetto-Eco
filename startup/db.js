58e10bcf26c9389b54ba190c905dbc2aeebf4f8d
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2022-01-21 15:04:29 +0100

    added token management for announcements and configuration update

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,18 @@ const config = require('config')
 mongoose.set('useFindAndModify', false)
 
 module.exports = function () {
-    const db = config.get('db')
+    let db = ""
+
+    if(config.get("docker_deploy") == "yes"){
+        db = config.get("db_docker")
+    }
+
+    else{
+        db = config.get("db")
+    }
+
+    // console.log("DB: "+db)
+
     mongoose.connect(db, { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
             .then(() => console.log('Successfully connected to MongoDB!'))
             .catch(err => console.log("Error encounterd while connecting to MongoDB : " , err.message))
